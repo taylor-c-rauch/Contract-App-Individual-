751f5a4557824e25cef6aa90570e63d2a6a1c2f6
Taylor Rauch <Taylor@d-172-25-79-118.dhcp.virginia.edu>
2018-06-04 13:59:37 -0400

    made styling changes, added cards to hold results and input fields, and changed alignment of text

--- contract/src/App.js
+++ contract/src/App.js
@@ -1,3 +1,4 @@
+//main app component
 import React, { Component } from "react";
 import "./App.css";
 import AppBar from "@material-ui/core/AppBar";
@@ -15,25 +16,29 @@ export default class App extends Component {
       details: ""
     };
   }
+  //this function will be passed to children components to update state in this component
   updateField(field, newVal) {
     this.setState({ [field]: newVal });
   }
+  //response when submit button (in contractForm.js) is clicked
   handleClick(e) {
     e.preventDefault();
+    //create object with info currently in state
     let form = {
       name: this.state.name,
       company: this.state.company,
       details: this.state.details
     };
+    //push new object to firebase
     const results = firebase.database().ref("contracts");
     results.push(form);
-    this.setState({ submitted: results });
+    //reset fields
     this.setState({ name: "", company: "", details: "" });
   }
 
   render() {
     return (
-      <center>
+      <div>
         <ContractForm
           update={(field, newVal) => this.updateField(field, newVal)}
           name={this.state.name}
@@ -41,10 +46,10 @@ export default class App extends Component {
           details={this.state.details}
           submit={e => this.handleClick(e)}
         />
-        <br />
+
         <br />
         <Display />
-      </center>
+      </div>
     );
   }
 }
